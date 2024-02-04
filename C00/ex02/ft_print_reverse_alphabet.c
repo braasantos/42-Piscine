748463e09233cd45ed55a5e381336c1a18d1b7b6
Braulio Jorge Monteiro Dos Santos <bjorge-m@c1r12s10.42porto.com>
2024-02-04 16:44:06 +0000

    Final Version

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjorge-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 12:27:52 by bjorge-m          #+#    #+#             */
+/*   Updated: 2024/02/04 16:24:24 by bjorge-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	int	i;
+
+	i = 'z';
+	while (i >= 'a')
+	{
+		write(1, &i, 1);
+		i--;
+	}
+}
+/*int	main(void)
+{
+	ft_print_reverse_alphabet ();
+	return (0);
+}*/
